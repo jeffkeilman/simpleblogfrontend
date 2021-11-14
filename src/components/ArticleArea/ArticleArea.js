77ffea542fe3eb6b929796f542a2d8ce5e7f3573
Jeff Keilman <jkeilman93@gmail.com>
2021-11-14 15:46:56 -0600

    Fix page height, add more test data

--- src/components/ArticleArea/ArticleArea.js
+++ src/components/ArticleArea/ArticleArea.js
@@ -6,13 +6,19 @@ const articles = [
     id: 1,
     posted_dt: '2021-11-09T22:15:41.118Z',
     title: 'My first article',
-    article_preview: "This is my first article. I'm writing it to learn about blogging. I would like to become a more proficient writer. I don't anticipate this blah blah yadda yadda doesn't matter just need some more length"
+    preview: "This is my first article. I'm writing it to learn about blogging. I would like to become a more proficient writer. I don't anticipate this blah blah yadda yadda doesn't matter just need some more length"
   },
   {
-    id: 1,
-    posted_dt: '2021-11-09T22:15:41.118Z',
-    title: 'My first article',
-    article_preview: "This is my first article. I'm writing it to learn about blogging. I would like to become a more proficient writer. I don't anticipate this blah blah yadda yadda doesn't matter just need some more length"
+    id: 2,
+    posted_dt: '2021-10-21T22:15:41.118Z',
+    title: 'My second article',
+    preview: "This is my second article. I'm writing it to learn about blogging. I would like to become a more proficient writer. I don't anticipate this blah blah yadda yadda doesn't matter just need some more length"
+  },
+  {
+    id: 3,
+    posted_dt: '2021-10-03T22:15:41.118Z',
+    title: 'My third article',
+    preview: "This is my third article. I'm writing it to learn about blogging. I would like to become a more proficient writer. I don't anticipate this blah blah yadda yadda doesn't matter just need some more length."
   }
 ]
 
@@ -34,11 +40,16 @@ class Description extends React.Component {
       return articles.map(article => {
         const articleDateStr = new Date(article.posted_dt).toDateString()
         const articleDateStrTrimmed = this.trimDate(articleDateStr)
+        const lastCharInPreview = article.preview[article.preview.length - 1]
+        if (lastCharInPreview === '.') {
+          // if last char in preview is a period, strip it from the preview so ... doesn't look weird
+          article.preview = article.preview.substring(0, article.preview.length - 1)
+        }
         return (
           <article key={article.id} className='Article'>
             <sup>Posted on: {articleDateStrTrimmed}</sup>
             <h1>{article.title}</h1>
-            <p>{article.article_preview}...</p>
+            <p>{article.preview}...</p>
           </article>
         )
       })
